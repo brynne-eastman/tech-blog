0b0b6bb07efd71ec8eefe47c31a80ce0e527cdc9
Brynne Eastman <brynne.eastman@gmail.com>
2021-11-26 11:55:13 -0500

    home-routes.js

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
@@ -21,10 +26,6 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -32,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+//app.use(require('./controllers'));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
