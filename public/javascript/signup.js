5046394cc61aae1b8dd9c512de10a23456a0fb4a
Brynne Eastman <brynne.eastman@gmail.com>
2021-11-26 19:52:50 -0500

    fixed error

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -2,7 +2,7 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
-    const username = document.querySelector('#password-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
         const response  = await fetch('/api/users', {
