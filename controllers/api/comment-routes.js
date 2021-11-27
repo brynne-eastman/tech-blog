448dffa276f206ef80dded269d5add5eb41ee1a1
Brynne Eastman <brynne.eastman@gmail.com>
2021-11-26 19:13:05 -0500

    changed db name as it was not working before

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
         post_id: req.body.post_id,
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { UPSERT } = require('sequelize/dist/lib/query-types');
 const sequelize = require('../../config/connection');
 const { Post, Uder, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
