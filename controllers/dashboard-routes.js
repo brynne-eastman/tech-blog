a10648b5b0b7d390250484db5c144acee71aa2f6
Brynne Eastman <brynne.eastman@gmail.com>
2021-11-26 21:51:07 -0500

    fixing errors

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -86,7 +86,9 @@ router.get('/edit/:id', withAuth, (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  res.render('new-post');
+  res.render('add-post', {
+    loggedIn: true
+  });
 });
   
 module.exports = router;
